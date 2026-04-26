598551e103b70e402e82348cf63fc520ad7c2754
README.md <tanishksood@gmail.com>
2026-04-26 13:38:05 +0530

    add try catch to user controllers

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -2,55 +2,47 @@ const bcrypt = require('bcrypt')
 const { User } = require('../models/user.models.js')
 const { generateCookie } = require('../utils/user.utils.js')
 
-
 // user register 
 const userRegister = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        let user = await User.findOne({ email });
 
-    const { name, email, password } = req.body;  // destructuring 
-    let user = await User.findOne({ email });  // from mongoDB
+        if (user) return res.status(404).json({
+            success: false,
+            message: "user already exist",
+        })
 
-    if (user) return res.status(404).json({
-        success: false,
-        message: "user already exist",
-    })
-
-    const hashPassword = await bcrypt.hash(password, 10);
-    user = await User.create({
-        name,
-        email,
-        password: hashPassword,  // updating password  with hashpassword 
-    });
-
-    generateCookie(user, res, 201, "User Register Successfully");
+        const hashPassword = await bcrypt.hash(password, 10);
+        user = await User.create({ name, email, password: hashPassword });
+        generateCookie(user, res, 201, "User Register Successfully");
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message })
+    }
 };
-
 
 // user login 
 const userLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        let user = await User.findOne({ email });
 
-    const { email, password } = req.body;
-    let user = await User.findOne({ email });
+        if (!user) return res.status(400).json({
+            success: false,
+            message: "user or email do not exist",
+        });
 
-    if (!user) return res.status(400).json({
-        success: false,
-        message: "user or email do not exist",
-    });
-
-    // user.password = comming from db 
-    // password      = comming from frontend website 
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-        return res.status(400).json({
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({
             success: false,
             message: "Invalid Credential or Wrong password"
         })
+
+        generateCookie(user, res, 201, `Welcome ${user.name}`);
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message })
     }
-
-    generateCookie(user, res, 201, `Welcome ${user.name}`);
 };
-
-
 
 // user logout 
 const userLogout = (req, res) => {
@@ -62,7 +54,6 @@ const userLogout = (req, res) => {
     })
 }
 
-
 // user profile details 
 const userProfile = async (req, res) => {
     res.status(200).json({
@@ -71,26 +62,23 @@ const userProfile = async (req, res) => {
     })
 }
 
-
 // finding user by id 
 const getUserById = async (req, res) => {
-    const id = req.params.id;
-    const user = await User.findById(id);
+    try {
+        const id = req.params.id;
+        const user = await User.findById(id);
 
-    if (!user) return res.status(404).json({
-        success: false,
-        message: "Invalid id, user does not exist "
-    })
+        if (!user) return res.status(404).json({
+            success: false,
+            message: "Invalid id, user does not exist "
+        })
 
-    res.json({
-        success: true,
-        message: "This  is the data of user ",
-        data: user
-    })
+        res.json({ success: true, message: "This is the data of user ", data: user })
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message })
+    }
 }
 
-
-// exporting 
 module.exports = {
     userRegister, userLogin, userLogout, userProfile, getUserById
 }
